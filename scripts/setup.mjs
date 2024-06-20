af2562ad0173bd4a900c897e5dcc402f0e73a4e3
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-06-20 16:35:15 +0530

    [CLEAN] Better naming

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -3,10 +3,7 @@ import { copyAllFiles } from "./copyFiles.mjs";
 import { generateSA } from "./generateSA.mjs";
 
 async function postInstall() {
-    console.log("Copying files")
     await copyAllFiles();
-    console.log("Copy files completed")
-    console.log("Generating service account.")
     await generateSA();
 }
 
