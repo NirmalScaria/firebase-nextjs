8a18db6be864bba0ad908f02442aa517042d12d8
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-06-21 20:57:38 +0530

    [FEATURE] Change declaration to module

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,35 @@
-import * as React from 'react';
+declare module 'nextfirejs/client/components' {
+    import * as React from 'react';
 
-interface AuthButtonProps {
-    children: React.ReactNode;
-    className?: string;
+    interface AuthButtonProps {
+        children: React.ReactNode;
+        className?: string;
+    }
+
+    interface EmailAuthButtonProps extends AuthButtonProps {
+        email: string;
+        password: string;
+        setErrorMessage: (message: string) => void;
+        setLoading?: (loading: boolean) => void;
+    }
+
+    interface ProfileButtonProps {
+        size?: number;
+    }
+
+    interface LogoutLogoProps {
+        height?: number;
+        width?: number;
+        [key: string]: any;
+    }
+
+    export function LogoutButton(props: AuthButtonProps): JSX.Element;
+
+    export function GoogleSignInButton(props: AuthButtonProps): JSX.Element;
+
+    export function EmailSignInButton(props: EmailAuthButtonProps): JSX.Element;
+
+    export function EmailSignUpButton(props: EmailAuthButtonProps): JSX.Element;
+
+    export function ProfileButton(props: ProfileButtonProps): JSX.Element;
 }
-
-interface EmailAuthButtonProps extends AuthButtonProps {
-    email: string;
-    password: string;
-    setErrorMessage: (message: string) => void;
-    setLoading?: (loading: boolean) => void;
-}
-
-interface ProfileButtonProps {
-    size?: number;
-}
-
-interface LogoutLogoProps {
-    height?: number;
-    width?: number;
-    [key: string]: any;
-}
-
-export function LogoutButton(props: AuthButtonProps): JSX.Element;
-
-export function GoogleSignInButton(props: AuthButtonProps): JSX.Element;
-
-export function EmailSignInButton(props: EmailAuthButtonProps): JSX.Element;
-
-export function EmailSignUpButton(props: EmailAuthButtonProps): JSX.Element;
-
-export function ProfileButton(props: ProfileButtonProps): JSX.Element;
