8c2a021919b43eeb739f31e0f08d229ddc52d8ad
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-06-21 22:30:41 +0530

    [WIP] Add types to auth

---
+++ client/auth.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "nextfirejs/nextfirejs-firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { User } from "firebase/auth";
+
+type NextFireJSContextType = {
+  userLoggedIn: boolean;
+  isEmailUser: boolean;
+  currentUser: User | null;
+};
+
+const NextFireJSContext = React.createContext<NextFireJSContextType>({
+  userLoggedIn: false,
+  isEmailUser: false,
+  currentUser: null,
+} as NextFireJSContextType);
+
+export function getUserCS() {
+  return useContext(NextFireJSContext);
+}
+
+export function NextFireJSProvider({ children }: { children: React.ReactNode }) {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [isEmailUser, setIsEmailUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+    return unsubscribe;
+  }, []);
+
+  async function initializeUser(user: User | null) {
+    if (user) {
+
+      setCurrentUser({ ...user });
+
+      const isEmail = user.providerData.some(
+        (provider) => provider.providerId === "password"
+      );
+      setIsEmailUser(isEmail);
+
+      user.getIdToken(true).then(function (idToken) {
+        document.cookie = `nextfirejs_token=${idToken}`;
+        // wait for 1 second
+      }).catch(function (error) {
+        console.error("FAILED TO GET ID TOKEN")
+      });
+
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+      document.cookie = "nextfirejs_token=";
+    }
+
+    setLoading(false);
+  }
+
+  const value = {
+    userLoggedIn,
+    isEmailUser,
+    currentUser
+  };
+
+  return (
+    <NextFireJSContext.Provider value={value}>
+      {loading ? <body /> : children}
+    </NextFireJSContext.Provider>
+  );
+}
