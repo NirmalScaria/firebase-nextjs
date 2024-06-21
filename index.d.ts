711c82419904a88b927c73802b2a85bf042a5389
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-06-21 20:54:58 +0530

    [FIX] Remove unnecessary import

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,33 @@
+import * as React from 'react';
 
-
-declare module 'nextfirejs/client/components' {
-    import * as React from 'react';
-    import { User } from 'firebase/auth';
-    interface AuthButtonProps {
-        children: React.ReactNode;
-        className?: string;
-    }
-
-    interface EmailAuthButtonProps extends AuthButtonProps {
-        email: string;
-        password: string;
-        setErrorMessage: ((message: string) => void);
-        setLoading?: ((loading: boolean) => void);
-    }
-
-    interface ProfileButtonProps {
-        size?: number;
-    }
-
-    interface LogoutLogoProps {
-        height?: number;
-        width?: number;
-        [key: string]: any;
-    }
-
-    export function LogoutButton(props: AuthButtonProps): JSX.Element;
-
-    export function GoogleSignInButton(props: AuthButtonProps): JSX.Element;
-
-    export function EmailSignInButton(props: EmailAuthButtonProps): JSX.Element;
-
-    export function EmailSignUpButton(props: EmailAuthButtonProps): JSX.Element;
-
-    export function ProfileButton(props: ProfileButtonProps): JSX.Element;
+interface AuthButtonProps {
+    children: React.ReactNode;
+    className?: string;
 }
+
+interface EmailAuthButtonProps extends AuthButtonProps {
+    email: string;
+    password: string;
+    setErrorMessage: (message: string) => void;
+    setLoading?: (loading: boolean) => void;
+}
+
+interface ProfileButtonProps {
+    size?: number;
+}
+
+interface LogoutLogoProps {
+    height?: number;
+    width?: number;
+    [key: string]: any;
+}
+
+export function LogoutButton(props: AuthButtonProps): JSX.Element;
+
+export function GoogleSignInButton(props: AuthButtonProps): JSX.Element;
+
+export function EmailSignInButton(props: EmailAuthButtonProps): JSX.Element;
+
+export function EmailSignUpButton(props: EmailAuthButtonProps): JSX.Element;
+
+export function ProfileButton(props: ProfileButtonProps): JSX.Element;
