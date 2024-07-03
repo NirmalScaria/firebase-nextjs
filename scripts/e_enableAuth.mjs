6548704690add8f7c6de8a6529723b4385d10d4f
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-07-03 14:27:25 +0530

    [FIX] Prompt to enable auth

--- scripts/e_enableAuth.mjs
+++ scripts/e_enableAuth.mjs
@@ -2,7 +2,10 @@
 
 import { google } from "googleapis";
 import { setupGoogleAuth } from "./d_setWebApp.mjs";
+import readline from "readline";
 const identitytoolkit = google.identitytoolkit('v2');
+import open from "open";
+import chalk from "chalk";
 
 export async function enableAuth(selectedProject) {
     await setupGoogleAuth(google)
@@ -10,18 +13,32 @@ export async function enableAuth(selectedProject) {
 }
 
 async function enableEmailPassword(selectedProject) {
-    const resp = await identitytoolkit.projects.updateConfig({
-        name: `projects/${selectedProject}/config`,
-        updateMask: "signIn.email.enabled,signIn.email.passwordRequired",
-        requestBody: {
-            signIn: {
-                email: {
-                    enabled: true,
-                    passwordRequired: true
+    try {
+        const resp = await identitytoolkit.projects.updateConfig({
+            name: `projects/${selectedProject}/config`,
+            updateMask: "signIn.email.enabled,signIn.email.passwordRequired",
+            requestBody: {
+                signIn: {
+                    email: {
+                        enabled: true,
+                        passwordRequired: true
+                    }
                 }
             }
-        }
-    })
+        })
+    }
+    catch (error) {
+        console.log("Please enable Authentication (Click 'Get Started') from Firebase Console. (You don't have to do any setup there)")
+        console.log(chalk.green("https://console.firebase.google.com/project/" + selectedProject + "/authentication"))
+        open("https://console.firebase.google.com/project/" + selectedProject + "/authentication")
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        await new Promise(resolve => {
+            rl.question("Press enter to continue ", resolve)
+        })
+        // Finally I get to use recursion for something useful.
+        await enableEmailPassword(selectedProject)
+    }
 }
-
-
