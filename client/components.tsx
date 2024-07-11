3e5a22381ce436d624c808f6ffdaab6e9541ab12
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-07-11 06:05:38 +0530

    [FEATURE] Login with redirect

--- client/components.tsx
+++ client/components.tsx
@@ -1,7 +1,7 @@
 "use client";
 import "./ProfileButtonStyle.css";
 import { doSignOut } from "../auth-actions";
-import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, User } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, User, signInWithRedirect } from "firebase/auth";
 import { auth } from "../firebasenextjs-firebase";
 import { getUserCS } from "./auth";
 import { decodeFirebaseError } from "./getFirebaseErrors";
@@ -22,7 +22,67 @@ export function LoggedOutContent({ children }: { children: React.ReactNode }) {
     return currentUser ? null : <>{children}</>;
 }
 
-export function GoogleSignInButton({ children, className }: { children?: React.ReactNode, className?: string }) {
+
+/**
+ * 
+ * !! IMPORTANT !!
+ * If using redirect method, additional configuration is required. Read examples below for more details.
+ * @param children - The component that should trigger the sign in with Google. If not provided, a default button will be shown.
+ * @param className - The class name of the button
+ * @param method - The method of sign in. Can be "popup" or "redirect". Default is "popup". Read examples for more details.
+ * 
+ * @returns  A component that triggers the sign in with Google.
+ * 
+ * @example
+ * // 1. Using default UI and popup
+ * // -------------------------------------
+ * <GoogleSignInButton />
+ * 
+ * @example
+ * // 2. Custom UI and popup
+ * // -------------------------------------
+ * <GoogleSignInButton>
+ *    <button className="bg-blue-500 text-white rounded-lg p-2">Sign in with Google</button>
+ * </GoogleSignInButton>
+ * 
+ * @example
+ * // 3. Using redirect
+ * // -------------------------------------
+ * 
+ * <GoogleSignInButton method="redirect" />
+ * 
+ * // SETUP (Required only for redirect method):
+ * // 1. Go to Firebase Console > Authentication > Settings > Authorized Domains
+ * // 2. Add your domain. (Add "localhost" to test locally).
+ * // 3. Open "firebase-app-config.json" and copy the authDomain value. (Ex: "your-app.firebaseapp.com")
+ * // 4. Edit nextjs.config.mjs and add the rewrite rules as shown below.
+ * 
+ * // nextjs.config.mjs
+ * const nextConfig = {
+ *   async rewrites() {
+ *       return [
+ *           {
+ *               source: '/__/auth/:path*',
+ *               destination: 'https://your-app.firebaseapp.com/__/auth/:path*',
+ *           },
+ *       ];
+ *   }
+ *};
+ *
+ * export default nextConfig;
+ * 
+ * // 5. Edit firebase-app-config.json and change the authDomain value to your app's domain.
+ * // Example: "authDomain": "your-website.com"
+ * // If locally: "authDomain": "localhost:3000"
+ * 
+ * // 6. Enforce https. (Required)
+ * // For localhost, this can be done by changing "next dev" in package.json to "next dev --experimental-https"
+ * 
+ * // If you come across any issue, please file the issue on GitHub.
+ * @link https://github.com/NirmalScaria/firebase-nextjs
+ * 
+ */
+export function GoogleSignInButton({ children, className, method = "popup" }: { children?: React.ReactNode, className?: string, method?: "popup" | "redirect" }) {
     const doSignInWithGoogle = async () => {
         const provider = new GoogleAuthProvider();
         const resp = await signInWithPopup(auth, provider);
@@ -33,11 +93,16 @@ export function GoogleSignInButton({ children, className }: { children?: React.R
         }
     };
 
+    const doSignInWithGoogleRedirect = async () => {
+        const provider = new GoogleAuthProvider();
+        signInWithRedirect(auth, provider);
+    };
+
     function GoogleLogo({ height = 24, width = 24, ...props }) {
         return <svg xmlns="http://www.w3.org/2000/svg" height={height} viewBox="0 0 24 24" width={width}><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" /><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" /><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" /><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" /><path d="M1 1h22v22H1z" fill="none" /></svg>
     }
 
-    return <div onClick={doSignInWithGoogle} className={className}>
+    return <div onClick={method == "popup" ? doSignInWithGoogle : doSignInWithGoogleRedirect} className={className}>
         {
             children ??
             <button className="w-full bg-white text-gray-500 font-medium rounded-lg p-2 text-sm border shadow-sm hover:shadow-md transition-all flex flex-row items-center justify-center gap-2">
--- dist/middleware.js
+++ dist/middleware.js
@@ -1,7 +1,7 @@
 import FirebaseNextJSMiddleware from "firebase-nextjs/middleware/firebase-nextjs-middleware";
 
 const options = {
-    allowRule: "^\/_next\/.*"
+    allowRule: "^\/_next\/|\/__\/auth\/.*" // Allow paths under /_next/ and /__/auth/ (for firebase auth) publically.
 }
 
 export default function middleware(req) {
